82065c55ab3bb8aee013ed5d20bb3c59356ab068
kyeongrok.kim <kyeongrok.kim@okiconcession.com>
2017-06-15 15:44:17 +0900

    중복되는 값 변수로 분리

--- src/main/com/main.py
+++ src/main/com/main.py
@@ -1,15 +1,17 @@
 from src.main.com import JsonParser
 from src.main.com.LogParser import LogParser
 
+eventId = "1703552"
+startDate = "2017-06-15"
 fileLocation = "C:/Users/kyeongrok.kim/Desktop/"
-fromFileName = "parse.log"
-targetFileName = "141000-1704623.txt"
-pattern = '2017-06-08 14:[1-2][1-9]:[0-9][0-9].*startTimestamp.*1704623.*'
+fromFileName = "parse_2017-06-15.87.log"
+targetFileName = "141000-"+eventId+".txt"
+pattern = startDate+" 14:[0-2][1-9]:[0-9][0-9].*startTimestamp.*"+eventId+".*"
 
 logParser = LogParser(fileLocation, fromFileName, targetFileName)
 logParser.extractPattern(pattern)
 logParser.replaePattern(targetFileName, "json.txt"
-                     , "2017-06-08 14:[1-2][1-9]:[0-9][0-9].*- {", "{")
+                     , "----", "{")
 
 jsonParser = JsonParser(fileLocation)
 jsonParser.readFiles("json.txt")
